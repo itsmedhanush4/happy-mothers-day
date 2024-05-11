62a00c13ed49f7d6adc4762e6a44f846f082bddb
itsmedhanush4 <130477244+itsmedhanush4@users.noreply.github.com>
2024-05-11 11:51:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function cake (){
+   document.getElementById("img").src = "cake.png";
+   document.getElementById("text").innerHTML = "You deserve the biggest cake";
+}
+
+function heart (){
+    document.getElementById("img").src= "heart.png";
+    document.getElementById("text").innerHTML = "In our hearts ❤️";
+}
+
+function draw(){
+   
+}
